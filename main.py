7694bc287c0043df08e2b2b90d4e879d0c6f4619
P202U <156369604+P202U@users.noreply.github.com.>
2025-11-25 17:56:32 +0530

    FIX error

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from linkedin import scrape_linkedin_jobs 
+from linkedin import scrape_linkedin_jobs
+import pandas as pd
 
-JOB_KEYWORD = 'data science'  
-JOB_LOCATION = 'Pune'
+JOB_KEYWORD = "data science"
+JOB_LOCATION = "Pune"
 PAGES_TO_GET = 1
 
 print(f"Starting scrape for '{JOB_KEYWORD}' jobs in '{JOB_LOCATION}'...")
 
-scraped_data_df = scrape_linkedin_jobs(
-    keyword=JOB_KEYWORD,
-    location=JOB_LOCATION,
-    max_pages=PAGES_TO_GET
+scraped_list = scrape_linkedin_jobs(
+    keyword=JOB_KEYWORD, location=JOB_LOCATION, max_pages=PAGES_TO_GET
 )
 
-print(scraped_data_df.head())
+scraped_df = pd.DataFrame(scraped_list)
+
+print(scraped_df.head())
